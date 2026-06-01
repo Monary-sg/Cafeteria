e59926c4199d0680735085d2e6d57451ed1252ee
brakam123 <krakz1234@gmail.com>
2026-05-31 21:10:48 -0700

    Se agregó módulo de artículos

---
+++ Modules/Articles/ArticlesModule.cs
@@ -0,0 +1,16 @@
+namespace Cafeteria.Modules.Articles
+{
+    public class ArticlesModule
+    {
+        public void Initialize()
+        {
+            // Se agregó módulo de artículos
+            System.Console.WriteLine("Módulo de artículos inicializado");
+        }
+
+        public void RegisterArticle(string name, decimal price)
+        {
+            // Registrar nuevo artículo
+        }
+    }
+}
